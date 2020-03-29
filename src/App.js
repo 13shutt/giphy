4535e4ed2ec4a3278dcf094d67b2188f27d00f76
13shutt <shutt13a@gmail.com>
2020-03-29 18:14:32 +0300

    Add css-reset and Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Giphy from './components/Giphy'
+import Loader from './components/Loader'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const App = () => (
-  <>
-    <CssBaseline />
-    <Header />
-    <Giphy />
-  </>
-)
+const App = () => {
+
+  const [viewLoader, setViewLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setViewLoader(false)
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      {viewLoader ? 
+      <Loader/> :
+      <>
+        <Header />
+        <Giphy />
+      </>
+      }
+    </>
+  )
+}
  
 export default App
--- src/components/Giphy/Giphy.js
+++ src/components/Giphy/Giphy.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid } from '@giphy/react-components'
 import { GiphyFetch } from '@giphy/js-fetch-api'
 
@@ -6,8 +6,10 @@ const gf = new GiphyFetch('Gxj4SH2RG6zmJtB97RTHjsJXsqfpgXpE')
 
 const fetchGifs = (offset) => gf.trending({ offset, limit: 10 })
 
-const Giphy = () => (
-  <Grid width={800} columns={4} fetchGifs={fetchGifs} />
-)
+const Giphy = () => {
+  return (
+    <Grid width={800} columns={4} fetchGifs={fetchGifs} />
+  )
+}
  
 export default Giphy
