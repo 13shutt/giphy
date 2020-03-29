84cec1b20f337d47898b2452ce697919a2e99549
13shutt <shutt13a@gmail.com>
2020-03-29 15:14:31 +0300

    Updates

---
+++ precache-manifest.60b4a2b02e0049bff8456532cffd6865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d39793e9f078f6633ddde4a51ebb2e6",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "dbcc7b0b96f48721570e",
+    "url": "/giphy/static/js/2.7cd52eef.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/giphy/static/js/2.7cd52eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb037aae6227f6ea164c",
+    "url": "/giphy/static/js/main.f17b4837.chunk.js"
+  },
+  {
+    "revision": "e7ad6a63007741f84a4e",
+    "url": "/giphy/static/js/runtime-main.79fb38c0.js"
+  }
+]);
