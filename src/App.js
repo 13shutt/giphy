7e3d39903ed1465af3632825b0a9344bf3610b28
13shutt <shutt13a@gmail.com>
2020-03-30 15:27:10 +0300

    Trouble with gifs fetching

--- src/App.js
+++ src/App.js
@@ -5,24 +5,36 @@ import Loader from './components/Loader'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import { GiphyFetch } from '@giphy/js-fetch-api'
+const gf = new GiphyFetch('Gxj4SH2RG6zmJtB97RTHjsJXsqfpgXpE')
+
 const App = () => {
 
-  const [viewLoader, setViewLoader] = useState(true);
+  const [viewLoader, setViewLoader] = useState(true)
+  const [gifs, setGifs] = useState([])
+
+  const fetchGifs = async (offset) => {
+    const data = await gf.trending({ offset, limit: 10 })
+    await setGifs(data)
+    console.log('fetch gifs', data, gifs)
+  }
 
   useEffect(() => {
+    fetchGifs()
     setTimeout(() => {
       setViewLoader(false)
-    }, 3000);
-  }, []);
+    }, 1500)
+  }, [])
 
   return (
     <>
+      {console.log(gifs, "gifs")}
       <CssBaseline />
       {viewLoader ? 
       <Loader/> :
       <>
         <Header />
-        <Giphy />
+        <Giphy fetchGifs={gifs} />
       </>
       }
     </>
--- src/components/Giphy/Giphy.js
+++ src/components/Giphy/Giphy.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Grid } from '@giphy/react-components'
-import { GiphyFetch } from '@giphy/js-fetch-api'
 
-const gf = new GiphyFetch('Gxj4SH2RG6zmJtB97RTHjsJXsqfpgXpE')
-
-const fetchGifs = (offset) => gf.trending({ offset, limit: 10 })
-
-const Giphy = () => {
+const Giphy = (props) => {
   return (
-    <Grid width={800} columns={4} fetchGifs={fetchGifs} />
+    <>
+      {console.log(props.fetchGifs, 'gihpy component')}
+      <Grid width={800} columns={3} fetchGifs={props.fetchGifs} />
+    </>
   )
 }
  
